684a696f03fd6deaa72b072234d630534ba2a1d8
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2024-04-29 21:35:05 +0100

    orderable votes

--- pages/vote/index.vue
+++ pages/vote/index.vue
@@ -12,7 +12,10 @@
   </div>
   <div id="submission-preview" class="hidden loading-fade">
     <div>
-      <div>Submission <span id="submission-progress">1</span> of {{ submissions.length }}</div>
+      <div class="step-heading">
+        <span>Step 1: Viewing Submissions</span>
+        <span>Submission <span id="submission-progress">1</span> of {{ submissions.length }}</span>
+      </div>
       <div>
         <button class="hidden">Back</button>
         <span></span>
@@ -21,6 +24,10 @@
     </div>
     <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 38 38" width="40" height="40"><defs><linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="A"><stop stop-color="#fff" stop-opacity="0" offset="0%"/><stop stop-color="#fff" stop-opacity=".631" offset="63.146%"/><stop stop-color="#fff" offset="100%"/></linearGradient></defs><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)"><path d="M36 18c0-9.94-8.06-18-18-18" stroke="url(#A)" stroke-width="2"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"/></path><circle fill="#fff" cx="36" cy="18" r="1"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"/></circle></g></g></svg>
     <img class="popupable">
+  </div>
+  <div id="submission-voting" class="hidden container">
+    <div class="step-heading">Step 2: Voting</div>
+    <div id="submission-list"></div>
   </div>
 </div>
 <div v-else class="container">Voting is not open at this time</div>
--- pages/vote/script.js
+++ pages/vote/script.js
@@ -1,10 +1,10 @@
 const submissions = document.getElementById("submissions")
 if (submissions) {
+  const images = shuffle(Array.from(submissions.content.children).map(e => ({ id: e.dataset.id, src: e.src })))
+  const preview = document.getElementById("submission-preview")
   document.getElementById("start").addEventListener("click", e => {
     document.getElementById("vote-start").classList.add("hidden")
-    const images = shuffle(Array.from(submissions.content.children).map(e => ({ id: e.dataset.id, src: e.src })))
-    let currentIndex = 0
-    const preview = document.getElementById("submission-preview")
+    let currentIndex = 13
     const back = preview.querySelector("button:first-child")
     const next = preview.querySelector("button:last-child")
     const img = preview.querySelector("img")
@@ -16,26 +16,30 @@ if (submissions) {
       loading = true
       if (!first) preview.classList.add("loading-fade")
       setTimeout(async () => {
-        first = false
-        img.src = images[currentIndex].src
-        preview.style.backgroundImage = `linear-gradient(#000C, #000C), url('${img.src}')`
-        const timeout = setTimeout(() => spinner.classList.add("visible"), 500)
-        await img.decode()
-        if (currentIndex <= 0) {
-          back.classList.add("hidden")
+        if (currentIndex === images.length) {
+          loadVoting()
         } else {
-          back.classList.remove("hidden")
+          first = false
+          img.src = images[currentIndex].src
+          preview.style.backgroundImage = `linear-gradient(#000C, #000C), url('${img.src}')`
+          const timeout = setTimeout(() => spinner.classList.add("visible"), 500)
+          await img.decode()
+          if (currentIndex <= 0) {
+            back.classList.add("hidden")
+          } else {
+            back.classList.remove("hidden")
+          }
+          if (currentIndex >= images.length - 1) {
+            next.textContent = "Vote"
+          } else {
+            next.textContent = "Next"
+          }
+          progress.textContent = currentIndex + 1
+          clearTimeout(timeout)
+          spinner.classList.remove("visible")
+          preview.classList.remove("loading-fade")
+          setTimeout(() => loading = false, 500)
         }
-        if (currentIndex >= images.length - 1) {
-          next.textContent = "Vote"
-        } else {
-          next.textContent = "Next"
-        }
-        progress.textContent = currentIndex + 1
-        clearTimeout(timeout)
-        spinner.classList.remove("visible")
-        preview.classList.remove("loading-fade")
-        setTimeout(() => loading = false, 500)
       }, first ? 0 : 500)
     }
     back.addEventListener("click", e => {
@@ -51,6 +55,81 @@ if (submissions) {
     showPreviewImage()
     preview.classList.remove("hidden")
   })
+  function loadVoting() {
+    preview.classList.add("hidden")
+    document.getElementById("submission-voting").classList.remove("hidden")
+    const list = document.getElementById("submission-list")
+    for (const [i, image] of images.entries()) {
+      const div = document.createElement("div")
+      div.className = "panel"
+      div.draggable = true
+      div.innerHTML = `
+        <div>#${i + 1}</div>
+        <img src="${image.src}" class="popupable"></div>
+        <div class="spacer"></div>
+        <div>
+          <div class="move-up icon">expand_less</div>
+          <div class="move-down icon">expand_more</div>
+        </div>
+      `
+      list.prepend(div)
+    }
+    function position() {
+      let top = 0
+      for (const [i, child] of Array.from(list.children).reverse().entries()) {
+        child.style.top = top + "px"
+        child.children[0].textContent = "#" + (i + 1)
+        top += child.clientHeight + 20
+      }
+      list.style.height = top + "px"
+    }
+    window.addEventListener("resize", position)
+    list.querySelectorAll("img").forEach(e => e.addEventListener("load", position))
+    list.querySelectorAll(".move-up").forEach(e => e.addEventListener("click", evt => {
+      e.parentNode.parentNode.parentNode.insertBefore(e.parentNode.parentNode.nextElementSibling, e.parentNode.parentNode)
+      position()
+    }))
+    list.querySelectorAll(".move-down").forEach(e => e.addEventListener("click", evt => {
+      e.parentNode.parentNode.parentNode.insertBefore(e.parentNode.parentNode, e.parentNode.parentNode.previousElementSibling)
+      position()
+    }))
+    let selected
+    document.addEventListener("dragover", e => e.preventDefault())
+    document.addEventListener("dragenter", e => e.preventDefault())
+    list.querySelectorAll(".panel").forEach(e => {
+      e.addEventListener("dragstart", evt => {
+        const img = new Image()
+        img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+        event.dataTransfer.setDragImage(img, 0, 0)
+        evt.dataTransfer.effectAllowed = "move"
+        evt.dataTransfer.setData("text/plain", null)
+        selected = evt.currentTarget
+        selected.classList.add("selected")
+      })
+      let processing
+      e.addEventListener("dragover", evt => {
+        if (processing) return
+        evt.dataTransfer.dropEffect = "grabbing"
+        if (isBefore(selected, evt.currentTarget)) {
+          processing = true
+          evt.currentTarget.parentNode.insertBefore(selected, evt.currentTarget)
+          setTimeout(() => position(), 10)
+          setTimeout(() => processing = false, 260)
+        } else if (isBefore(evt.currentTarget, selected)) {
+          processing = true
+          evt.currentTarget.parentNode.insertBefore(selected, evt.currentTarget.nextSibling)
+          setTimeout(() => position(), 10)
+          setTimeout(() => processing = false, 260)
+        }
+      })
+      e.addEventListener("dragend", evt => {
+        selected.classList.remove("selected")
+        selected = null
+      })
+    })
+  }
+  document.getElementById("vote-start").classList.add("hidden")
+  loadVoting()
 }
 
 function shuffle(arr) {
@@ -61,4 +140,14 @@ function shuffle(arr) {
     arr.splice(index, 1)
   }
   return out
+}
+
+function isBefore(el1, el2) {
+  let cur
+  if (el2.parentNode === el1.parentNode) {
+    for (cur = el1.previousSibling; cur; cur = cur.previousSibling) {
+      if (cur === el2) return true
+    }
+  }
+  return false
 }

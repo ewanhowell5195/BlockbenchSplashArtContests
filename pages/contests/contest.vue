7abc23499a2f0d43120c647c418b163d0fdf95e6
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2024-05-01 13:04:27 +0100

    style contest page

--- pages/contests/contest.vue
+++ pages/contests/contest.vue
@@ -2,40 +2,48 @@
   <div></div>
   <h1>Splash Art Contest {{ currentContest.id }}</h1>
   <h2>"{{ currentContest.theme }}"</h2>
-  <h3 v-if="currentContest.status === 'upcoming'">Submissions open {{ f.relativeTime(currentContest.open) }}</h3>
-  <h3 v-else-if="currentContest.status === 'submissions'">Submissions close {{ f.relativeTime(currentContest.close) }}</h3>
-  <h3 v-else-if="currentContest.status === 'reviewing'">Voting opens {{ f.relativeTime(currentContest.vote) }}</h3>
-  <h3 v-else-if="currentContest.status === 'voting'">Voting closes {{ f.relativeTime(currentContest.finish) }}</h3>
-  <h3 v-else-if="currentContest.status === 'finished'">For Blockbench v{{ currentContest.version }} - The {{ currentContest.name }} Update</h3>
 </div>
 <div v-if="currentContest.status !== 'finished'" class="container">
   <div class="panel">
+    <h2>Welcome to the {{ f.numSuffix(currentContest.id) }} Blockbench Splash Art Contest!</h2>
+    <p>This is the contest for the upcoming Blockbench update v{{ currentContest.version }}!</p>
+    <h3 v-if="currentContest.status === 'upcoming'">Submissions open {{ f.relativeTime(currentContest.open) }}</h3>
+    <h3 v-else-if="currentContest.status === 'submissions'">Submissions close {{ f.relativeTime(currentContest.close) }}</h3>
+    <h3 v-else-if="currentContest.status === 'reviewing'">Voting opens {{ f.relativeTime(currentContest.vote) }}</h3>
+    <h3 v-else-if="currentContest.status === 'voting'">Voting closes {{ f.relativeTime(currentContest.finish) }}</h3>
+    <h3 v-else-if="currentContest.status === 'finished'">For Blockbench v{{ currentContest.version }} - The {{ currentContest.name }} Update</h3>
     <button v-if="currentContest.status === 'upcoming'" disabled>Submit your splash art!</button>
     <a v-else-if="currentContest.status === 'submissions'" class="button" :href="'/submission'">Submit your splash art!</a>
     <button v-else-if="currentContest.status === 'reviewing'" disabled>Vote now!</button>
     <a v-else-if="currentContest.status === 'voting'" class="button" :href="'/vote'">Vote now!</a>
   </div>
-  <h2>Welcome to the {{ f.numSuffix(currentContest.id) }} Blockbench Splash Art Contest!</h2>
-  <p>This is the contest for the upcoming Blockbench update {{ currentContest.version }}!</p>
-  <h2>Prizes</h2>
-  <ul>
-    <li>The <strong>top voted 20%</strong> of submissions (max 5) will be featured in order as a slide show on the Blockbench splash screen</li>
-    <li>The <strong>1st place</strong> will be featured as the update art on social media posts about the update!</li>
-    <li>The <strong>1st place</strong> model will also be featured in the <a href="https://www.blockbench.net/gallery" target="_blank">Blockbench Gallery</a></li>
-    <li>The splash screen winners will receive a special Discord role with its own section in the member list for 4 weeks after the contest!</li>
-  </ul>
-  <p>The Theme for this Contest: <strong>Underwater!</strong></p>
-  <h2>Rules</h2>
-  <ol>
-    <li>Renders can be made in Blender, Cinema4D, Light Tracer, Sketchfab, or in similar programs. Image editing in post (contrast, backgrounds etc.) is allowed. <a href="https://www.blockbench.net/wiki/guides/model-rendering" target="_blank">Model rendering basics</a></li>
-    <li>The model needs to be an original creation, made in Blockbench for this contest. Don't re-use existing models or textures for the model or environment (that includes game worlds as backgrounds)</li>
-    <li>Submission should not be based on existing brands or IPs</li>
-    <li>Submissions should be G rated (no violence etc.)</li>
-    <li>Collaborations of up to two people are allowed</li>
-    <li>You can't win (1st place) in two consecutive contests</li>
-    <li>Only one submission per person</li>
-  </ol>
-  <h2>Good luck and enjoy!</h2>
+  <div class="panel">
+    <h2>The Theme for this Contest: <strong>Underwater!</strong></h2>
+  </div>
+  <div class="panel">
+    <h2>Prizes</h2>
+    <ul>
+      <li>The <strong>top voted 20%</strong> of submissions (max 5) will be featured in order as a slide show on the Blockbench splash screen.</li>
+      <li>The <strong>1st place</strong> will be featured as the update art on social media posts about the update!</li>
+      <li>The <strong>1st place</strong> model will also be featured in the <a href="https://www.blockbench.net/gallery" target="_blank">Blockbench Gallery</a></li>
+      <li>The splash screen winners will receive a special Discord role with its own section in the member list for 4 weeks after the contest!</li>
+    </ul>
+  </div>
+  <div class="panel">
+    <h2>Rules</h2>
+    <ol>
+      <li>Renders can be made in Blender, Cinema4D, Light Tracer, Sketchfab, or in similar programs. Image editing in post (contrast, backgrounds etc.) is allowed. <a href="https://www.blockbench.net/wiki/guides/model-rendering" target="_blank">Model rendering basics</a></li>
+      <li>The model needs to be an original creation, made in Blockbench for this contest. Don't re-use existing models or textures for the model or environment (that includes game worlds as backgrounds)</li>
+      <li>Submission should not be based on existing brands or IPs</li>
+      <li>Submissions should be G rated (no violence etc.)</li>
+      <li>Collaborations of up to two people are allowed</li>
+      <li>You can't win (1st place) in two consecutive contests</li>
+      <li>Only one submission per person</li>
+    </ol>
+  </div>
+  <div class="panel">
+    <h2>Good luck and enjoy!</h2>
+  </div>
 </div>
 <div v-if="currentContest.status === 'finished'" class="container">
   <p v-if="currentContest.description">{{ currentContest.description }}</p>

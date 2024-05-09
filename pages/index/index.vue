c4ae0d66a62f4ebc01baf1950af68d64c2257882
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2024-05-09 13:10:31 +0100

    new homepage

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -11,20 +11,15 @@
   <div v-for="[i, contest] of contests.slice(0, 3).entries()" :i="i" class="home-content" :class="{ enter: i }">
     <h2>Splash Art Contest {{ contest.id }}</h2>
     <h1 :style="{ backgroundImage: f.blurImageSVG(contest.status === 'finished' ? `assets/images/submissions/${contest.id}/${contest.image}_thumbnail_small.webp` : `assets/images/contests/concept_${contest.id}_thumbnail_small.webp`) }">{{ contest.theme }}</h1>
-    <div v-if="contest.status !== 'finished'" class="live-details">
+    <div v-if="contest.status !== 'finished'" id="live-details">
       <div class="live">Live</div>
-      <p><strong>Welcome to the {{ f.numSuffix(contest.id) }} Blockbench Splash Art Contest!</strong></p>
-      <h3 v-if="contest.status === 'upcoming'">Submissions open {{ f.relativeTime(contest.open) }}</h3>
-      <h3 v-else-if="contest.status === 'submissions'">Submissions close {{ f.relativeTime(contest.close) }}</h3>
-      <h3 v-else-if="contest.status === 'reviewing'">Voting opens {{ f.relativeTime(contest.vote) }}</h3>
-      <h3 v-else-if="contest.status === 'voting'">Voting closes {{ f.relativeTime(contest.finish) }}</h3>
-    </div>
-    <div v-if="contest.status === 'finished'">
-      <p>The splash art contest has concluded and the results are in!</p>
+      <div v-if="contest.status === 'upcoming'">Submissions open {{ f.relativeTime(contest.open) }}</div>
+      <div v-else-if="contest.status === 'submissions'">Submissions close {{ f.relativeTime(contest.close) }}</div>
+      <div v-else-if="contest.status === 'reviewing'">Voting opens {{ f.relativeTime(contest.vote) }}</div>
+      <div v-else-if="contest.status === 'voting'">Voting closes {{ f.relativeTime(contest.finish) }}</div>
     </div>
     <div class="button-row">
-      <a v-if="contest.status !== 'finished'" class="button" :href="'contests/' + contest.id">View contest</a>
-      <a v-else class="button" :href="'contests/' + contest.id">View results</a>
+      <a class="button" :href="'contests/' + contest.id">View contest</a>
       <a class="button secondary" href="contests">View older contests</a>
     </div>
   </div>

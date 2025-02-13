0f7a70cefcd84234b69dda9ed1bdcdc4be05f0d7
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2025-02-13 18:19:19 +0000

    better 404 page

--- views/404.vue
+++ views/404.vue
@@ -1,1 +1,24 @@
-<h1>Nothing here!</h1>
+<style>
+  main {
+    display: flex;
+    align-items: center;
+
+    .container {
+      align-items: center;
+      justify-content: center;
+    }
+
+    h1 {
+      margin: 0 0 16px;
+    }
+
+    h3 {
+      margin: 0;
+    }
+  }
+</style>
+<div class="container">
+  <h3>404</h3>
+  <h1>Page not found</h1>
+  <a class="button" href="/contests">Check out the contests instead!</a>
+</div>
